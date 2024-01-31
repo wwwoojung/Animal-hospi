41031b66e765e039c8b5c534eb38b9704e61fc7f
wwwoojung <50447560+wwwoojung@users.noreply.github.com>
2024-01-31 18:19:41 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,14 @@ $(function () {
     const MAIN_VISUAL_SLIDE = new Swiper('.MainVisual .main_visual_slide', {
         loop: true,
     })
+
+    $('.MainCategory .tab_menu li a').on('click', function (e) {
+        e.preventDefault();
+        const IDX = $(this).parent().index();
+        $('.MainCategory .tab_menu li').removeClass('on');
+        $('.MainCategory .tab_menu li').eq(IDX).addClass('on');
+
+        $('.MainCategory .tab_content .itm').removeClass('on');
+        $('.MainCategory .tab_content .itm').eq(IDX).addClass('on');
+    })
 })
